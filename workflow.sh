e948a3b2dc23a954e22cff0213deb42699c15231
B192186-2021 <email_not_provided>
2021-10-20 06:41:56 +0100

    script1 is ready

---
+++ workflow.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+### USAGE: ./workflow.sh [multi_threads] [NEW_data_directory] [NEW_reference] [NEW_bedfile]
+
+################################## Manualy Configuration ########################################
+# defalt threads for this script is 4
+thread=4
+
+### initialize the defalt path for input data
+
+# directory containing the pair-end RNAseq raw reads data 
+dataD="/localdisk/data/BPSM/AY21/fastq"
+
+# reference genome in fasta.gz format
+ref="/localdisk/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz"
+
+# bedfile containing annotation data
+ann="/localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed"
+
+####################################### Preperation ##############################################
+# if these paths are updated to different files or directories
+if [ -n "$2" ]; then dataD=$2; echo "dataD="$2; fi
+if [ -n "$3" ]; then ref=$3; echo "ref="$3; fi
+if [ -n "$4" ]; then ann=$4; echo "ann="$4; fi
+
+# if a different thread number are specified
+if [ -n "$1" ]; then thread=$1; echo "thread="$1; fi
+##################################################################################################
+
+# copy the reference data to the present directory
+mkdir ref
+cp $ref ref/
